118d3530c22c519e5751ac9ce3d2ce5eb1961cb8
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-10-13 13:50:51 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,9 +6,15 @@ if [ -z "$RUNNER_TOKEN" ]; then
   exit 1
 fi
 
+# Check if the RUNNER_TOKEN is set
+if [ -z "$RUNNER_REPO" ]; then
+  echo "Error: RUNNER_TOKEN is not set."
+  exit 1
+fi
+
 # Run the config script with the token
 echo "Configuring runner with token..."
-./config.sh --url https://github.com/HexaEngine --token $RUNNER_TOKEN
+./config.sh --url https://github.com/$RUNNER_REPO --token $RUNNER_TOKEN
 
 
 ./run.sh
